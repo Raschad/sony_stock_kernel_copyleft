598cd3035fd5744716fe9d54a03c8f46c462a51e
munjeni <mystopspammail@example.com>
2017-09-23 14:49:11 +0200

    [PATCH] kernel: Only expose su when daemon is running
    It has been claimed that the PG implementation of 'su' has security
    vulnerabilities even when disabled.  Unfortunately, the people that
    find these vulnerabilities often like to keep them private so they
    can profit from exploits while leaving users exposed to malicious
    hackers.
    
    In order to reduce the attack surface for vulnerabilites, it is
    therefore necessary to make 'su' completely inaccessible when it
    is not in use (except by the root and system users).
    
    Change-Id: Ia7d50ba46c3d932c2b0ca5fc8e9ec69ec9045f85

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -93,6 +93,12 @@ struct sched_param {
 
 #include <asm/processor.h>
 
+int  su_instances(void);
+bool su_running(void);
+bool su_visible(void);
+void su_exec(void);
+void su_exit(void);
+
 struct exec_domain;
 struct futex_pi_state;
 struct robust_list_head;
@@ -1840,6 +1846,8 @@ static inline int is_global_init(struct task_struct *tsk)
 {
 	return tsk->pid == 1;
 }
+
+#define PF_SU		0x80000000      /* task is su */
 
 /*
  * is_container_init:
